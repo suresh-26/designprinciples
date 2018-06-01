a1e8a0c176762c7b621e2d75d6a8ca2240a844cb
Suresh <sureshchandra.emb@gmail.com>
2018-06-01 09:43:01 +0700

    Update HealthInsuranceSurveyor.java

--- src/com/oodesign/ocp/violating/HealthInsuranceSurveyor.java
+++ src/com/oodesign/ocp/violating/HealthInsuranceSurveyor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright (c) Rakuten, Inc. All Rights Reserved.
- * 
- * This program is the information assets which are handled
- * as "Strictly Confidential".
- * Permission of Use is only admitted in Rakuten Inc.
- * Development Department.
- * If you don't have permission , MUST not be published,
- * broadcast, rewritten for broadcast or publication
- * or redistributed directly or indirectly in any medium.
- * 
- * $Id$
- * Created on Apr 18, 2016
- * Updated on $Date$
- */
+
 package com.oodesign.ocp.violating;
 
 /**
